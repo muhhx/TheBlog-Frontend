64c9872156c154a782db5d3609441a139eedc7e1
Murilo Santos <muriloue@gmail.com>
2022-07-11 21:44:24 -0300

    Production setup

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,10 +1,6 @@
 export default {
-  BASE_SERVER_URL:
-    // @ts-ignore
-    process.env.VITE_APP_BASE_SERVER_URL || "http://localhost:5000",
-  // @ts-ignore
-  GOOGLE_CLIENT_ID: process.env.VITE_APP_GOOGLE_CLIENT_ID as string,
-  // @ts-ignore
-  GOOGLE_OAUTH_REDIRECT_URL: process.env
+  BASE_SERVER_URL: "https://theblog-social-backend.herokuapp.com/",
+  GOOGLE_CLIENT_ID: import.meta.env.VITE_APP_GOOGLE_CLIENT_ID as string,
+  GOOGLE_OAUTH_REDIRECT_URL: import.meta.env
     .VITE_APP_GOOGLE_OAUTH_REDIRECT_URL as string,
 };
