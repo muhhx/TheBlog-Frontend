864d18f1bf433e75f64d1fa8c21d1ea72bb8b7da
Murilo Santos <muriloue@gmail.com>
2022-07-11 21:37:42 -0300

    Production setup

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,5 +1,6 @@
 export default {
-  BASE_SERVER_URL: import.meta.env.BASE_SERVER_URL || "http://localhost:5000",
+  BASE_SERVER_URL:
+    import.meta.env.VITE_APP_BASE_SERVER_URL || "http://localhost:5000",
   GOOGLE_CLIENT_ID: import.meta.env.VITE_APP_GOOGLE_CLIENT_ID as string,
   GOOGLE_OAUTH_REDIRECT_URL: import.meta.env
     .VITE_APP_GOOGLE_OAUTH_REDIRECT_URL as string,
